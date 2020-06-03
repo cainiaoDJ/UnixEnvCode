f16a6f528a551fccf25b2637ffe292d077bbc472
yanghuiwen <cainiaodj@qq.com>
2020-06-03 18:59:25 +0800

    不同case下的exit值

---
+++ c8_6.c
@@ -0,0 +1,48 @@
+//
+// Created by 杨慧文 on 2020/6/3.
+//
+
+#include "apue.h"
+#include "sys/wait.h"
+
+int main(void) {
+    pid_t pid;
+    int status;
+
+    // child
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        exit(7);
+    }
+
+    // wait for child, print its status
+    if (wait(&status) != pid) {
+        err_sys("wait error");
+    }
+    pr_exit(status);
+
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        abort();
+    }
+
+    if (wait(&status) != pid) {
+        err_sys("wait error");
+    }
+    pr_exit(status);
+
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        status /= 0;
+    }
+
+    if (wait(&status) != pid) {
+        err_sys("wait error");
+    }
+    pr_exit(status);
+
+    exit(0);
+}
