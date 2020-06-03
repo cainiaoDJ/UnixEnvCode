29d05b367f75c1491a958693206818551ac1c660
yanghuiwen <cainiaodj@qq.com>
2020-06-03 19:15:51 +0800

    带有竞争条件的fork

---
+++ c8_12.c
@@ -0,0 +1,40 @@
+//
+// Created by 杨慧文 on 2020/6/3.
+//
+
+//  似乎在MAC下，输出不会被串
+#include "apue.h"
+
+static void charatatime(char *);
+
+int main(void) {
+    pid_t pid;
+
+    // ++++
+    TELL_WAIT();
+    // ++++
+
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        // ++++
+        WAIT_PARENT();
+        // ++++
+        charatatime("output from child \n");
+    } else {
+        charatatime("output from parent \n");
+        // ++++
+        TELL_CHILD(pid);
+        // ++++
+    }
+    exit(0);
+}
+
+static void charatatime(char *str) {
+    char *ptr;
+    int c;
+    setbuf(stdout, NULL);
+    for (ptr = str; (c = *ptr++) != 0;) {
+        putc(c, stdout);
+    }
+}
