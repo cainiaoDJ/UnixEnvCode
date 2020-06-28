161e0d945aedc5c1b10212b28a1a8f1c0ec69e00
root <root@192-168-115-133.localdomain>
2020-06-28 15:49:07 +0800

    使用longjmp，带时间限制的read

---
+++ c10_11.c
@@ -0,0 +1,39 @@
+#include "apue.h"
+#include <setjmp.h>
+
+static void sig_alrm(int);
+static jmp_buf env_alrm;
+
+int main(void)
+{
+    int n;
+    char line[MAXLINE];
+    if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+    {
+        err_sys("signal(SIGALRM) error");
+    }
+
+    // 跳转入口处
+    if (setjmp(env_alrm) != 0)
+    {
+        err_quit("read timeout");
+    }
+
+    alarm(10);
+
+    if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
+    {
+        err_sys("read error");
+    }
+
+    alarm(0);
+
+    write(STDIN_FILENO, line, n);
+    exit(0);
+}
+
+// 信号被触发的时候，longjmp 执行跳转。
+static void sig_alrm(int signo)
+{
+    longjmp(env_alrm, 1);
+}
