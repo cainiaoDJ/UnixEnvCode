8a848806109b9f28cd1d5755b9899b73796f0597
root <root@192-168-115-134.localdomain>
2020-06-29 20:05:11 +0800

    保护临界区不被信号中断

---
+++ c10_22.c
@@ -0,0 +1,50 @@
+#include "apue.h"
+
+static void sig_int(int);
+
+int main(void)
+{
+    sigset_t newmask, oldmask, waitmask;
+    pr_mask("program start: ");
+
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+    {
+        err_sys("signal(SIGINT) error");
+    }
+    sigemptyset(&waitmask);
+    sigaddset(&waitmask, SIGUSR1);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGINT);
+
+    // block SIGINT and save current signal mask.
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+    {
+        err_sys("SIG_BLOCK error");
+    }
+
+    // critical region of code
+    pr_mask("in critical region:");
+
+    // pasue, allowing all signals except SIGUSR1
+    if (sigsuspend(&waitmask) != -1)
+    {
+        err_sys("sigsuspend error");
+    }
+
+    pr_mask("after return from sigsuspend: ");
+
+    // reset signal mask which unblocks SIGINT.
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+    {
+        err_sys("SIG_SETMASK error");
+    }
+
+    // and continue processing
+    pr_mask("program exit: ");
+    exit(0);
+}
+
+static void sig_int(int signo)
+{
+    pr_mask("\n in sig_int:");
+}
