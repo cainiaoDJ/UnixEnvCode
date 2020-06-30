10c4a6ec89876fddb93419dbf74ef3abc91e3038
root <root@192-168-115-134.localdomain>
2020-06-30 10:28:43 +0800

    添加注释

--- c10_22.c
+++ c10_22.c
@@ -11,21 +11,25 @@ int main(void)
     {
         err_sys("signal(SIGINT) error");
     }
+
+    // 添加信号集，waitmask里是SIGUSR1，newmask里是SIGINT
     sigemptyset(&waitmask);
     sigaddset(&waitmask, SIGUSR1);
     sigemptyset(&newmask);
     sigaddset(&newmask, SIGINT);
 
     // block SIGINT and save current signal mask.
+    // 添加屏蔽信号，为newmask 与 oldmask并集
     if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
     {
         err_sys("SIG_BLOCK error");
     }
 
-    // critical region of code
+    // critical region of code，因为只有SIGINT被加入
     pr_mask("in critical region:");
 
     // pasue, allowing all signals except SIGUSR1
+    // 暂停，等待，当sigsuspend返回的时候，信号屏蔽字被设为之前的值，也就是说不响应USR1
     if (sigsuspend(&waitmask) != -1)
     {
         err_sys("sigsuspend error");
